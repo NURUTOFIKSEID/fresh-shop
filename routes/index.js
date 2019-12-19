0011af7896723225ab77bd230d1484254b865aef
nurutofikseid <tofikseid313@gmail.com>
2019-12-19 19:15:45 +0800

    full project done

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
-
+const User = require('../models/User');
 // Welcome Page
 router.get('/', forwardAuthenticated, (req, res) => res.render('index'));
 
@@ -12,6 +12,38 @@ router.get('/dashboard', ensureAuthenticated, (req, res) =>
     user: req.user
   })
 );
+
+router.get("/edit", (req, res) => res.render("edit", {
+  user: req.user
+}));
+router.get("/setting", (req, res) => res.render("setting", {
+  user: req.user
+}));
+
+router.post("/edit", (req, res) => {
+  const {
+    name,
+    email
+  } = req.body;
+  const id = {
+    _id: req.body.id
+  };
+  const updateItem = {
+    $set: {
+      name: name,
+      email: email
+    }
+  };
+  User.updateOne(id, updateItem, (err) => {
+    if (err) {
+      console.log(err);
+
+    } else {
+      res.redirect("/setting");
+      console.log("Settings updated");
+    }
+  });
+});
 
 router.post("/delete", (req, res) => {
   const deleteItem = req.body.id;
